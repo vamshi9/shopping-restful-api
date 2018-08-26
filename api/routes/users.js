3527fd5c0638c0a8fe1939a152f3b1391c13a5d1
vamshi9 <vamsisai999@gmail.com>
2018-08-26 20:56:23 +0530

    jwt set up

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose= require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user'); 
 
@@ -81,8 +82,20 @@ router.post('/login', (req,res,next) => {
           });
         }
         if(result){
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId:user[0]._id
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "2hr"
+            }
+          )
+
           return res.status(200).json({
-            message: 'Authentication Successfull, mama!'
+            message: 'Authentication Successfull, mama!',
+            token: token
           });
         }
         res.status(401).json({
